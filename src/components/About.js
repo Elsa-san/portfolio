a179fbde951aee0f1ebbdd87dcd99e0218e9ca1b
Elsa-san <elsa.tartary@gmail.com>
2023-11-23 11:46:47 +0100

    colors modification

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ class About extends Component {
     return (
       <section id="about">
         <div className="col-md-12">
-          <h1 style={{ color: "black" }}>
+          <h1 style={{ color: "#404040ff" }}>
             <span>{sectionName}</span>
           </h1>
           <div className="row center mx-auto mb-5">
